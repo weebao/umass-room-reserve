da26e4c6958a4525e51580438483a233c8e483bf
Tristesse02 <mtvu@umass.edu>
2024-04-25 18:27:06 -0400

    update on datacard

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,5 +1,7 @@
 import { Events } from "../Events.js";
 import { Search } from "../components/Search.js";
+import { DataCard } from "../components/DataCard.js";
+import { buildings } from "../mock/mockdata.js";
 
 export class HomePage {
   #events = null;
@@ -20,8 +22,26 @@ export class HomePage {
     `;
 
     const search = new Search();
+    const dataCard = new DataCard();
+
     const searchElm = await search.render();
+    let dataCardElm = await dataCard.render({buildings: [], rooms: []});
     elm.appendChild(searchElm);
+
+    // This is so stupid I actually have to find another way
+    let timeoutId;
+    searchElm.querySelector("#search-input").addEventListener("input", async (event) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(async () => {
+        if (elm.querySelector("#grid-container")) elm.removeChild(dataCardElm);
+        if (event.target.value === "") return; // if user dont have any input we return directly
+
+        dataCardElm = await dataCard.render(await search.search(event.target.value))
+        
+        elm.appendChild(dataCardElm);
+      }, 1000);
+    });
+
 
     return elm;
   }
